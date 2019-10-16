b76f5d4e326d68cc970187123f4ff1fccc065138
robmalec <robmalec@umich.edu>
2019-10-16 16:58:31 -0400

    Fixed a big mistake and added some input checking

--- src/co/grandcircus/Lab4.java
+++ src/co/grandcircus/Lab4.java
@@ -6,8 +6,10 @@ public class Lab4 {
 	static int numColWidth = -1, sqColWidth = -1, cuColWidth = -1, minColWidth = 8;
 	public static void main(String[] args) {
 		Boolean done = false;
-		Scanner scn = new Scanner(System.in);
+		final Scanner scn = new Scanner(System.in);
 		int num = -1;
+		int seanCounter = 0;
+		
 		do
 		{
 			try {
@@ -40,22 +42,52 @@ public class Lab4 {
 				System.out.println("Continue? (y/n)");
 				char resp = scn.next().charAt(0);
 				if (resp == 'n') {
-					done = false;
+					done = true;
 				}
 				else if (resp != 'y') {
 					throw new Exception();
 				}
-				
-			}
-			catch (Exception e)
-			{
-				System.out.println("I'm sorry Sean, I'm afraid you can't do that.");
 				scn.nextLine();
 			}
+			catch (Exception e){
+				seanCounter++;
+				yellAtSean(seanCounter);
+				scn.nextLine();
+			}
+			int z = 0;
 		} while (done == false);
+		System.out.println("Goodbye.");
 	}
-	public static void makeHeader() {
-		
+	
+	public static void yellAtSean(int seanCounter) {
+		String whining = "";
+		switch (seanCounter) {
+		case 1:
+			whining = "I'm sorry Sean, I'm afraid you can't do that.";
+			break;
+		case 2:
+			whining = "Come on now.  Get your " + genRandChars(4) + " together.";
+			break;
+		case 3:
+			whining = "Seriously?  You think this is funny or something?";
+			break;
+		default:
+			whining = genRandChars((int)Math.pow(2.0, (double) seanCounter));
+			for (int i = 0; i < seanCounter; i++) {
+				whining += "!";
+			}
+			break;
+		}
+		System.out.println(whining);
+	}
+	
+	public static String genRandChars(int length) {
+		String retString = "";
+		for (int i = 0; i < length; i++) {
+			int thisChar = 33 + (int) (Math.random() * 14.0);
+			retString += ((char) thisChar);
+		}
+		return retString;
 	}
 	
 	public static void calcAndPrintLine(int num) {
